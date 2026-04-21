be3a896d880347ff2993bfc05c47fa02c8ac3f87
Sanskruti Gite <sanskrutigite6532@gmail.com>
2026-04-21 11:23:38 +0530

    blogs and search features

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
 from .models import Blog,Category
+from django.db.models import Q
 
 # Create your views here.
 
@@ -9,15 +10,36 @@ def posts_by_category(request,category_id):
     #fetch the posts that belong to the category with the id category_id
     
     posts=Blog.objects.filter(status='Published',category_id = category_id)
+    
+    
     # try:                                                           #this whole try and catch shows u correct category page and if the categor page dosen't exist it redirect u to home
     #     category=Category.objects.get(pk=category_id)
     # except:
     #     return redirect('home')
-    category=get_object_or_404(Category,id=category_id)   #this basically shows page not found to user
+    category=get_object_or_404(Category,id=category_id)   #this basically shows page not found to user and with this we r using 404.html file 
+    
+    
     
     context={
         'posts':posts,
         'category':category,
     }
     return render(request,'posts_by_category.html',context)
+
+def blogs(request,slug):
+    single_blog=get_object_or_404(Blog,slug=slug,status='Published')
+    context={
+        'single_blog':single_blog,
+    }
+    return render(request,'blogs.html',context)
+
+
+def search(request):
+    keyword= request.GET.get('keyword')
     
+    blogs=Blog.objects.filter(Q(title__icontains=keyword)| Q(short_description__icontains=keyword)| Q(blog_body__icontains=keyword),status='Published')
+    context={
+        'blogs':blogs,
+        'keyword':keyword,
+    }
+    return render(request,'search.html',context)
