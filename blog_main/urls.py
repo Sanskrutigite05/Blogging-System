b268637a1395bca81cdea880e23f12a73f56c9ea
Sanskruti Gite <sanskrutigite6532@gmail.com>
2026-04-28 19:49:03 +0530

    category functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -32,6 +32,5 @@ urlpatterns = [
     path('register/', views.register,name='register'),
     path('login/', views.login ,name='login'),
     path('logout/', views.logout ,name='logout'),
-
-    
+    path('dashboard/',include('dashboards.urls')),    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)      #It tells Django:“Whenever someone accesses a URL starting with MEDIA_URL, serve files from the MEDIA_ROOT folder.”
